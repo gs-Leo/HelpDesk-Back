1a33881f74c1e3c6590b67c507671a828fe7b8a1
Leo <leonardogds015@gmail.com>
2022-06-30 23:11:11 -0300

    Adicionado a data Abertura

--- src/services/registerCardService.js
+++ src/services/registerCardService.js
@@ -6,6 +6,7 @@ class InsertCard{
     async insert(card) {
         card.id = randomUUID()
         card.status = true;
+        card.dataAbertura = new Date().toLocaleString('pt-BR', { timeZone: 'America/Sao_Paulo' });
         const resultCard = await conn("Cards").insert(card);
         
         return resultCard;        
